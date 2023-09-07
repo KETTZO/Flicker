ed11db6845da020b140477c69006a033e558fb4b
Joshan1394 <105181432+Joshan1394@users.noreply.github.com>
2023-09-06 18:49:59 -0600

    feat: navbar, footer y sidebar
    Diseño del navbar, footer y sidebar para las vistas

---
+++ Project-Pinterest/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+	SidebarContainer,
+	Icon,
+	CloseIcon,
+	SidebarMenu,
+	SidebarLink,
+	
+} from './SideBarElements';
+
+function Sidebar({ isOpen, toggle }) {
+	return (
+		<SidebarContainer isOpen={isOpen}>
+			<Icon onClick={toggle}>
+				<CloseIcon />
+			</Icon>
+			<SidebarMenu>
+				<SidebarLink to='/'>Home</SidebarLink>
+				<SidebarLink to='/'>Inicio de sesión</SidebarLink>
+				<SidebarLink to='/'>Perfil</SidebarLink>
+			
+			</SidebarMenu>
+			{/* <SideBtnWrap>
+				<SidebarRoute to='/'>Order Now</SidebarRoute>
+			</SideBtnWrap> */}
+		</SidebarContainer>
+	);
+}
+
+export default Sidebar;
